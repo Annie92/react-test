9a2336d19b647a5e891ccad662f1f3cc2664dc23
anniep <annie.pappachan@trantorinc.com>
2025-04-29 15:31:53 +0530

    Added an input for searching movie. Onchange of input, set the search term state. Now check the react extension and in MoviesGrid component state will get updated

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,25 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import '../styles.css';
-import MovieCard from './MovieCard';
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
-    const [ movies, setMovies] = useState([]);
-    useEffect( () => {
-        fetch('movies.json')
-            .then( response => response.json())
-            .then( data => setMovies(data));
-    }, []);
-    return(
-        <div>
-            <span>Total movies: {movies.length}</span>
-            <div className='movies-grid'>
-                {
-                    movies.map( movie => (
-                        <MovieCard movie={movie} key={movie.id}></MovieCard>
-                    ))
-                }
-                    
-            </div>
-        </div>
-    );
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  const handleSearchChange = (e) => {
+    console.log(e.target.value);
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="search movies..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <span>Total movies: {movies.length}</span>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
+    </div>
+  );
 }
